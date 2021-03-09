f78a3d496cb848aef0604491592bf445e24eb06b
daviidy <yaodavidarmel@gmail.com>
2021-03-09 12:28:02 +0000

    implemented buttonpanel, button and display components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Display />
       <ButtonPanel />
-    </React.Fragment>
+    </>
   );
 }
 
