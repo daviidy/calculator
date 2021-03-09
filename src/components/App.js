f8fb525f216f488a4015785f07b450915b01b191
daviidy <yaodavidarmel@gmail.com>
2021-03-09 09:25:26 +0000

    implemented App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Display />
+      <ButtonPanel />
+    </React.Fragment>
+  );
+}
+
+export default App;
